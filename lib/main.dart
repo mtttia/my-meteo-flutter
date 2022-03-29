0c2c59b8998a28584a018faa0cca5e8f38e7bb28
mtttia <tia369852@gmail.com>
2022-03-29 23:14:29 +0200

    updated application theme

--- lib/main.dart
+++ lib/main.dart
@@ -26,17 +26,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My meteo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primaryColor: background),
+      theme: ThemeData.light().copyWith(
+        primaryColor: lightBlue,
+        backgroundColor: background,
+        scaffoldBackgroundColor: background,
+        
+      ),
       home: App(),
     );
   }
